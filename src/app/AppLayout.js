8ed001176ce437e454bd5148f62fdf79b243e881
Mateusz Otorowski <mati1otorowski@gmail.com>
2024-01-23 19:17:45 +0100

    I prepared a style for the entire search bar

--- src/app/AppLayout.js
+++ src/app/AppLayout.js
@@ -1,32 +1,65 @@
 import { NavLink, Outlet } from "react-router-dom";
 import ROUTES from "./routes";
+import RESOURCES from "../data/resources";
+import styles from "../styles/AppLayout.css";
 
 const AppLayout = () => {
 
-    return (
-        <div>
-            <nav>
-                <ul>
-                    <li>
-                        <NavLink to={ROUTES.homeRoute()}>
-                            Home
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to={ROUTES.popularRoute()}>
-                            Popular
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to={ROUTES.selectedTopicRoute("LeagueOfLegends")}>
-                            League of Legends
-                        </NavLink>
-                    </li>
-                </ul>
-            </nav>
-            <Outlet />
+  return (
+    <div className="container">
+      <div className="logo-search-bar">
+        <div className="logo">
+          <img
+            className="logo-img"
+            src={RESOURCES.redditLogo}
+            alt="Logo Reddit" />
         </div>
-    )
+        <form>
+          <input
+            type="text"
+            placeholder="search anything" />
+          <button
+            type="submit"
+            className="form-button">
+            <img
+              src={RESOURCES.searchButton}
+              alt="search button" />
+          </button>
+        </form>
+        <div>
+          <a href={RESOURCES.cv} download>
+            <button className="resume">
+              Get Resume
+            </button>
+          </a>
+        </div>
+      </div>
+      <button className="separator" />
+      {/* 
+      <div className="category-menu">
+        <ul>
+          <li>
+            <NavLink to={ROUTES.homeRoute()}>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to={ROUTES.popularRoute()}>
+              Popular
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to={ROUTES.selectedTopicRoute("LeagueOfLegends")}>
+              League of Legends
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+      <div className="posts-container">
+        <Outlet />
+      </div> */}
+    </div>
+  )
 };
 
 export default AppLayout;
---
+++ src/data/resources.js
@@ -0,0 +1,11 @@
+import searchButton from "../images/search.png"
+import redditLogo from "../images/reddit-logo.png"
+import cv from "../cv/Mateusz_Otorowski_CV_aktualne.pdf";
+
+const RESOURCES = {
+ searchButton: searchButton,
+ redditLogo: redditLogo,
+ cv: cv,
+}
+
+export default RESOURCES;
